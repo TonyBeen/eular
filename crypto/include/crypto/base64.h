8435fdf779815239a0e95857cea968441db02d0f
TonyBeen <1772747354@qq.com>
2025-07-11 15:49:04 +0800

    update

--- crypto/include/crypto/base64.h
+++ crypto/include/crypto/base64.h
@@ -8,16 +8,48 @@
 #ifndef __CRYPTO_BASE64_H__
 #define __CRYPTO_BASE64_H__
 
+#include <stdint.h>
+#include <string>
+#include <vector>
+#include <memory>
+
+#define base64_encode_len(A) (((A) + 2) / 3 * 4)
+#define base64_decode_len(A) ( (A) / 4 * 3 + 2)
+
 namespace eular {
 namespace crypto {
-
+class Base64Context;
 class Base64
 {
+    Base64(const Base64&) = delete;
+    Base64& operator=(const Base64&) = delete;
 public:
     Base64();
     ~Base64();
-};
 
+    Base64(Base64 &&other);
+    Base64& operator=(Base64 &&other);
+
+    static std::string Encrypt(const void *data, size_t len);
+    template<typename T>
+    static std::string Encrypt(const std::basic_string<T> &data) {
+        return Encrypt(data.data(), data.size() * sizeof(T));
+    }
+    template<typename T>
+    static std::string Encrypt(const std::vector<T> &data) {
+        return Encrypt(data.data(), data.size() * sizeof(T));
+    }
+
+    static std::vector<uint8_t> Decrypt(const void *data, size_t len);
+    static std::vector<uint8_t> Decrypt(const std::string &data) {
+        return Decrypt(data.data(), data.size());
+    }
+
+    std::string encrypt(const void *data, size_t len);
+    std::string encryptFinal();
+private:
+    std::unique_ptr<Base64Context>  m_context;
+};
 } // namespace crypto
 } // namespace eular
 
---
+++ crypto/src/base64.cpp
@@ -0,0 +1,102 @@
+/*************************************************************************
+    > File Name: base64.cpp
+    > Author: hsz
+    > Brief:
+    > Created Time: 2025年07月11日 星期五 15时00分15秒
+ ************************************************************************/
+
+#include "base64.h"
+
+#include "libbase64.h"
+
+namespace eular {
+namespace crypto {
+class Base64Context {
+public:
+    struct base64_state  _ctx;
+};
+
+Base64::Base64()
+{
+}
+
+Base64::~Base64()
+{
+    m_context.reset();
+}
+
+Base64::Base64(Base64 &&other)
+{
+    std::swap(m_context, other.m_context);
+}
+
+Base64& Base64::operator=(Base64 &&other)
+{
+    if (this != std::addressof(other)) {
+        std::swap(m_context, other.m_context);
+    }
+
+    return *this;
+}
+
+std::string Base64::Encrypt(const void *data, size_t len)
+{
+    if (data == nullptr || len == 0) {
+        return std::string();
+    }
+    size_t size = base64_encode_len(len);
+    std::string result;
+    result.resize(size);
+    base64_encode((const char *)data, len, const_cast<char *>(result.data()), &size, 0);
+    result.resize(size);
+    return result;
+}
+
+std::vector<uint8_t> Base64::Decrypt(const void *data, size_t len)
+{
+    if (data == nullptr || len == 0) {
+        return std::vector<uint8_t>();
+    }
+
+    size_t size = base64_decode_len(len);
+    std::vector<uint8_t> result;
+    result.resize(size);
+    base64_decode((const char *)data, len, reinterpret_cast<char *>(result.data()), &size, 0);
+    result.resize(size);
+    return result;
+}
+
+std::string Base64::encrypt(const void *data, size_t len)
+{
+    if (data == nullptr || len == 0) {
+        return std::string();
+    }
+    if (m_context == nullptr) {
+        m_context = std::unique_ptr<Base64Context>(new Base64Context());
+        base64_stream_encode_init(&m_context->_ctx, 0);
+    }
+    std::string result;
+    auto size = base64_encode_len(len);
+    result.resize(size);
+    base64_stream_encode(&m_context->_ctx, (const char *)data, len,
+                         const_cast<char *>(result.data()), &size);
+    result.resize(size);
+    return result;
+}
+
+std::string Base64::encryptFinal()
+{
+    if (m_context == nullptr) {
+        return std::string();
+    }
+
+    std::string result;
+    char buffer[8] = {0};
+    size_t size = sizeof(buffer);
+    base64_stream_encode_final(&m_context->_ctx, buffer, &size);
+    result.assign(buffer, size);
+    return result;
+}
+
+} // namespace crypto
+} // namespace eular
