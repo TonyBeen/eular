79881df6ee97bcac569d4f2f19d9bb49a75f274a
TonyBeen <1772747354@qq.com>
2025-07-11 14:53:47 +0800

    update

---
+++ crypto/include/crypto/base64.h
@@ -0,0 +1,24 @@
+/*************************************************************************
+    > File Name: base.h
+    > Author: hsz
+    > Brief:
+    > Created Time: 2025年07月11日 星期五 11时58分55秒
+ ************************************************************************/
+
+#ifndef __CRYPTO_BASE64_H__
+#define __CRYPTO_BASE64_H__
+
+namespace eular {
+namespace crypto {
+
+class Base64
+{
+public:
+    Base64();
+    ~Base64();
+};
+
+} // namespace crypto
+} // namespace eular
+
+#endif // __CRYPTO_BASE64_H__
