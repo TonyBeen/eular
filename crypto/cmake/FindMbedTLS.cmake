8ec9138df6c207a901c78f4924ae2362a0e77a2c
TonyBeen <1772747354@qq.com>
2025-07-18 10:20:48 +0800

    mbedtls 增加查找静态库

--- crypto/cmake/FindMbedTLS.cmake
+++ crypto/cmake/FindMbedTLS.cmake
@@ -65,7 +65,7 @@ The following cache variables may also be set:
 
 ``MbedTLS_INCLUDE_DIR``
   The directory containing ``mbedtls/aes.h``.
-``MbedTLS_LIBRARY``
+``MbedTLS_SHARED_LIBRARY``
   The path to the Mbed TLS library.
 
 #]=======================================================================]
@@ -75,67 +75,103 @@ The following cache variables may also be set:
 #pkg_check_modules(PC_MbedTLS QUIET mbedtls)
 
 find_path(MbedTLS_INCLUDE_DIR
-  NAMES mbedtls/aes.h
-#  PATHS ${PC_MbedTLS_INCLUDE_DIRS}
+    NAMES mbedtls/aes.h
+    # PATHS ${PC_MbedTLS_INCLUDE_DIRS}
 )
-find_library(MbedTLS_LIBRARY
-  NAMES mbedcrypto
-#  PATHS ${PC_MbedTLS_LIBRARY_DIRS}
+
+set(shared_suffix)
+if(WIN32)
+    set(shared_suffix ".dll")
+else()
+    set(shared_suffix ".so")
+endif()
+
+find_library(MbedTLS_SHARED_LIBRARY
+    NAMES
+        "mbedcrypto${shared_suffix}"   # mbedcrypto.dll (Win)
+        "libmbedcrypto${shared_suffix}" # libmbedcrypto.so (Unix)
+    # PATHS ${PC_MbedTLS_LIBRARY_DIRS}
 )
+
+message(STATUS "MbedTLS_SHARED_LIBRARY: ${MbedTLS_SHARED_LIBRARY}")
+
+set(static_suffix)
+if(WIN32)
+    set(static_suffix ".lib")
+else()
+    set(static_suffix ".a")
+endif()
+
+find_library(MbedTLS_StTATIC_LIBRARY
+    NAMES
+        "mbedcrypto${static_suffix}"   # mbedcrypto.lib (Win)
+        "libmbedcrypto${static_suffix}" # libmbedcrypto.a (Unix)
+    # PATHS ${PC_MbedTLS_LIBRARY_DIRS}
+)
+
+message(STATUS "MbedTLS_StTATIC_LIBRARY: ${MbedTLS_StTATIC_LIBRARY}")
 
 # Extract version information from the header file
 if(MbedTLS_INCLUDE_DIR)
-  # for major version 3
-  if(EXISTS ${MbedTLS_INCLUDE_DIR}/mbedtls/build_info.h)
-    file(STRINGS ${MbedTLS_INCLUDE_DIR}/mbedtls/build_info.h _ver_line
-        REGEX "^#define MBEDTLS_VERSION_STRING  *\"[0-9]+\\.[0-9]+\\.[0-9]+\""
-        LIMIT_COUNT 1)
-    string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+"
-        MbedTLS_VERSION "${_ver_line}")
-    unset(_ver_line)
-  # for major version 2
-  elseif(EXISTS ${MbedTLS_INCLUDE_DIR}/mbedtls/version.h)
-    file(STRINGS ${MbedTLS_INCLUDE_DIR}/mbedtls/version.h _ver_line
-         REGEX "^#define MBEDTLS_VERSION_STRING  *\"[0-9]+\\.[0-9]+\\.[0-9]+\""
-         LIMIT_COUNT 1)
-    string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+"
-           MbedTLS_VERSION "${_ver_line}")
-    unset(_ver_line)
-  else()
-    if(PC_MbedTLS_VERSION)
-      set(MbedTLS_VERSION ${PC_MbedTLS_VERSION})
+    # for major version 3
+    if(EXISTS ${MbedTLS_INCLUDE_DIR}/mbedtls/build_info.h)
+        file(STRINGS ${MbedTLS_INCLUDE_DIR}/mbedtls/build_info.h _ver_line
+            REGEX "^#define MBEDTLS_VERSION_STRING  *\"[0-9]+\\.[0-9]+\\.[0-9]+\""
+            LIMIT_COUNT 1)
+        string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+"
+            MbedTLS_VERSION "${_ver_line}")
+        unset(_ver_line)
+    # for major version 2
+    elseif(EXISTS ${MbedTLS_INCLUDE_DIR}/mbedtls/version.h)
+        file(STRINGS ${MbedTLS_INCLUDE_DIR}/mbedtls/version.h _ver_line
+            REGEX "^#define MBEDTLS_VERSION_STRING  *\"[0-9]+\\.[0-9]+\\.[0-9]+\""
+            LIMIT_COUNT 1)
+        string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+"
+            MbedTLS_VERSION "${_ver_line}")
+        unset(_ver_line)
     else()
-      # version unknown
-      set(MbedTLS_VERSION "0.0")
+        if(PC_MbedTLS_VERSION)
+        set(MbedTLS_VERSION ${PC_MbedTLS_VERSION})
+        else()
+        # version unknown
+        set(MbedTLS_VERSION "0.0")
+        endif()
     endif()
-  endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MbedTLS
-  FOUND_VAR MbedTLS_FOUND
-  REQUIRED_VARS
-    MbedTLS_LIBRARY
-    MbedTLS_INCLUDE_DIR
-  VERSION_VAR MbedTLS_VERSION
+    FOUND_VAR MbedTLS_FOUND
+    REQUIRED_VARS
+        MbedTLS_SHARED_LIBRARY
+        MbedTLS_INCLUDE_DIR
+    VERSION_VAR MbedTLS_VERSION
 )
 
 if(MbedTLS_FOUND)
-  set(MbedTLS_LIBRARIES ${MbedTLS_LIBRARY})
-  set(MbedTLS_INCLUDE_DIRS ${MbedTLS_INCLUDE_DIR})
-#  set(MbedTLS_DEFINITIONS ${PC_MbedTLS_CFLAGS_OTHER})
+    set(MbedTLS_LIBRARIES ${MbedTLS_SHARED_LIBRARY})
+    set(MbedTLS_INCLUDE_DIRS ${MbedTLS_INCLUDE_DIR})
+    # set(MbedTLS_DEFINITIONS ${PC_MbedTLS_CFLAGS_OTHER})
 endif()
 
-if(MbedTLS_FOUND AND NOT TARGET MbedTLS::MbedTLS)
-  add_library(MbedTLS::MbedTLS UNKNOWN IMPORTED)
-  set_target_properties(MbedTLS::MbedTLS PROPERTIES
-    IMPORTED_LOCATION "${MbedTLS_LIBRARY}"
-#    INTERFACE_COMPILE_OPTIONS "${PC_MbedTLS_CFLAGS_OTHER}"
-    INTERFACE_INCLUDE_DIRECTORIES "${MbedTLS_INCLUDE_DIR}"
-  )
+if(MbedTLS_FOUND AND NOT TARGET MbedTLS::Shared)
+    add_library(MbedTLS::Shared  UNKNOWN IMPORTED)
+    set_target_properties(MbedTLS::Shared PROPERTIES
+        IMPORTED_LOCATION "${MbedTLS_SHARED_LIBRARY}"
+        # INTERFACE_COMPILE_OPTIONS "${PC_MbedTLS_CFLAGS_OTHER}"
+        INTERFACE_INCLUDE_DIRECTORIES "${MbedTLS_INCLUDE_DIR}"
+    )
+endif()
+
+if(NOT TARGET MbedTLS::Static AND MbedTLS_StTATIC_LIBRARY)
+    add_library(MbedTLS::Static STATIC IMPORTED)
+    set_target_properties(MbedTLS::Static PROPERTIES
+        IMPORTED_LOCATION "${MbedTLS_StTATIC_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${MbedTLS_INCLUDE_DIR}"
+    )
 endif()
 
 mark_as_advanced(
-  MbedTLS_INCLUDE_DIR
-  MbedTLS_LIBRARY
-  )
+    MbedTLS_INCLUDE_DIR
+    MbedTLS_SHARED_LIBRARY
+)
